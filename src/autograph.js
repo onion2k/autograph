1df7f8e70920ab5f8e6826556753583a41bf3dfc
Chris <chris@usablehq.com>
2017-12-19 11:04:42 +0000

    Working in react context

--- src/autograph.js
+++ src/autograph.js
@@ -1,57 +1,69 @@
+import React, { Component } from "react";
 
-	function autograph(a,b,o) {
+class Autograph extends Component {
+  constructor() {
+    super();
+    this.draw = this.draw.bind(this);
+    this.state = {
+      a: 7,
+      b: 3,
+      o: 5
+    };
+  }
+  draw() {
+    this.ctx.clearRect(0, 0, 600, 600);
 
-		var canvas = document.getElementById('autograph');
+    this.ctx.lineWidth = 0.75;
+    this.ctx.lineJoin = "round";
+    this.ctx.beginPath();
 
-		if (canvas.getContext){
-			var ctx = canvas.getContext('2d');
+    let x1 = 0;
+    let y1 = 0;
 
-			ctx.clearRect(0,0,460,460);
+    var width = 600;
+    var height = 600;
+    var cog_a = parseFloat(this.state.a);
+    var cog_b = parseFloat(this.state.b);
+    var i = parseFloat(this.state.o);
 
-			ctx.lineWidth = 0.5;
-			ctx.lineJoin = 'round';
-			ctx.beginPath();
+    var xoffset = width / 2;
+    var yoffset = height / 2;
+    var ra = i;
+    var rb = width / 2 - i;
+    var xi = (x1 = ra * Math.cos(cog_a) + rb * Math.cos(cog_b));
+    var yi = (y1 = ra * Math.sin(cog_a) + rb * Math.sin(cog_b));
+    var ta = cog_a;
+    var tb = cog_b;
 
-			//User settings
-			var width = 920;
-			var height = 920;
-			var dots=1;
-			var cog_a=parseFloat(a);
-			var cog_b=parseFloat(b);
-			var i=parseFloat(o);
+    this.ctx.moveTo(x1 + xoffset, y1 + yoffset);
 
-			var downscale = 2;
+    do {
+      cog_a += ta;
+      cog_b += tb;
+      x1 = ra * Math.cos(cog_a) + rb * Math.cos(cog_b);
+      y1 = ra * Math.sin(cog_a) + rb * Math.sin(cog_b);
 
-			//Other stuff
-			var xoffset = width/2;
-			var yoffset = height/2;
-			var ra=(i);
-			var rb=(((width)/2)-i);
-			var xi=x1=((ra*Math.cos(cog_a))+(rb*Math.cos(cog_b)));
-			var yi=y1=((ra*Math.sin(cog_a))+(rb*Math.sin(cog_b)));
-			var ta=cog_a;
-			var tb=cog_b;
-			var c=0;
+      this.ctx.lineTo(x1 + xoffset, y1 + yoffset);
+    } while (!(Math.abs(x1 - xi) < 2 && Math.abs(yi - y1) < 2));
 
-			ctx.moveTo((x1+xoffset)/downscale,(y1+yoffset)/downscale);
+    this.ctx.stroke();
+  }
 
-			do {
+  componentDidMount() {
+    this.ctx = this.canvas.getContext("2d");
+    this.draw();
+  }
+  render() {
+    return (
+      <canvas
+        ref={canvas => {
+          this.canvas = canvas;
+        }}
+        width="600"
+        height="600"
+      />
+    );
+  }
+}
 
-				c++;
-
-				x2=x1;
-				y2=y1;
-				cog_a+=ta;
-				cog_b+=tb;
-				x1=((ra*Math.cos(cog_a))+(rb*Math.cos(cog_b)));
-				y1=((ra*Math.sin(cog_a))+(rb*Math.sin(cog_b)));
-
-				ctx.lineTo((x1+xoffset)/downscale,(y1+yoffset)/downscale);
-
-			}while (!((Math.abs(x1-xi)<2)&&(Math.abs(yi-y1)<2)));
-
-			ctx.stroke();
-
-		}
-
-	}
+export default Autograph;
